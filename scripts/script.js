5115fded1c324691bd6224be994be012fce57e6a
Donghoon Oh <06-sailor-calmest@icloud.com>
2023-10-18 10:38:13 -0700

    add authentication

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
-//---------------------------------
-// Your own functions here
-//---------------------------------
+// //---------------------------------
+// // Your own functions here
+// //---------------------------------
 
 
-function sayHello() {
-    //do something
+// function sayHello() {
+//     //do something
+// }
+// //sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
 }
-//sayHello();    //invoke function
